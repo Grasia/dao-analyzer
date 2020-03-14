334cfb2d8d4c707fa0ea1205f1486bee3d6be52f
FRYoussef <f.r.youssef@hotmail.com>
2020-03-14 12:41:54 +0100

    WIP dao stacked serie test

---
+++ test/unit/apps/daostack/data_access/test_dao_stacked_serie.py
@@ -0,0 +1,20 @@
+"""
+   Descp: Tester for DaoStackedSerie.
+
+   Created on: 14-mar-2020
+
+   Copyright 2020-2021 Youssef 'FRYoussef' El Faqir El Rhazoui
+        <f.r.youssef@hotmail.com>
+"""
+
+import unittest
+from hypothesis import given, settings, strategies as st
+from typing import List, Dict
+
+
+class DaoStackedSerieTest(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
