d7bd4886fc77c836d6999078a89a7e5ec02f4db2
FRYoussef <f.r.youssef@hotmail.com>
2020-03-25 09:28:07 +0100

    Fixed env var issue

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@
 import os
 import dash
 
-DEBUG = os.environ['DEBUG'] == 'TRUE'
+DEBUG = os.environ['DEBUG'] == 'TRUE' if 'DEBUG' in os.environ else False
 
 app = dash.Dash(__name__)
 server = app.server
