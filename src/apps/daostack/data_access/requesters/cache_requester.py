8d41f4d1ecd79efd9b647cb2e6922849764656b6
FRYoussef <f.r.youssef@hotmail.com>
2020-07-16 13:58:15 +0200

    added cache checker as method

--- src/apps/daostack/data_access/requesters/cache_requester.py
+++ src/apps/daostack/data_access/requesters/cache_requester.py
@@ -9,6 +9,7 @@
 
 import pandas as pd
 import os
+from typing import List
 
 from src.apps.daostack.data_access.requesters.irequester import IRequester
 from src.logs import LOGS
@@ -19,6 +20,7 @@ PROPOSALS: str = os.path.join(CACHE_PATH, 'proposals.csv')
 REP_HOLDERS: str = os.path.join(CACHE_PATH, 'reputation_holders.csv')
 STAKES: str = os.path.join(CACHE_PATH, 'stakes.csv')
 VOTES: str = os.path.join(CACHE_PATH, 'votes.csv')
+ALL_FILES: List[str] = [DAOS, PROPOSALS, REP_HOLDERS, STAKES, VOTES]
 
 
 class CacheRequester(IRequester):
@@ -39,3 +41,17 @@ class CacheRequester(IRequester):
             return pd.DataFrame() 
         
         return pd.read_csv(path, header=0)
+
+
+    @classmethod
+    def is_cache_available(cls) -> bool:
+        """
+        Checks whether the cache is available.
+        Return:
+            True if it is, False if it is not.
+        """
+        available: bool = True
+        for filename in CACHE_PATH:
+            available &= os.path.isfile(filename)
+
+        return available
