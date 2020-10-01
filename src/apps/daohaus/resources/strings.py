79a165ec30326f266a7cd58ee6509a787fe6b352
FRYoussef <f.r.youssef@hotmail.com>
2020-10-01 12:06:21 +0200

    Added DAOhaus folder schema

---
+++ src/apps/daohaus/resources/strings.py
@@ -0,0 +1,11 @@
+"""
+   Descp: This file is used to store the DAOhaus app's text.
+
+   Created on: 1-oct-2020
+
+   Copyright 2020-2021 Youssef 'FRYoussef' El Faqir El Rhazoui
+        <f.r.youssef@hotmail.com>
+"""
+
+TEXT: dict = {
+}
