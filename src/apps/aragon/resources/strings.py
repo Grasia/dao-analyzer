a7ef382dd1b586b48005037e3d5ca4cb14e6a7e5
FRYoussef <f.r.youssef@hotmail.com>
2020-10-16 12:20:47 +0200

    Added Aragon app folder structure

---
+++ src/apps/aragon/resources/strings.py
@@ -0,0 +1,13 @@
+"""
+   Descp: This file is used to store Aragon app's text.
+
+   Created on: 16-oct-2020
+
+   Copyright 2020-2021 Youssef 'FRYoussef' El Faqir El Rhazoui
+        <f.r.youssef@hotmail.com>
+"""
+
+TEXT: dict = {
+    'css_pane_border': 'aragon-border',
+    'pane_css_prefix': 'aragon-pane',
+}
