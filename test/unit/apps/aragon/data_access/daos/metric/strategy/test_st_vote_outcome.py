8253112deb3dfaa7c6aa7cc7de14d2bf7e841418
FRYoussef <f.r.youssef@hotmail.com>
2020-10-30 17:03:45 +0100

    Fixed aragon proposals outcome bug

--- test/unit/apps/aragon/data_access/daos/metric/strategy/test_st_vote_outcome.py
+++ test/unit/apps/aragon/data_access/daos/metric/strategy/test_st_vote_outcome.py
@@ -35,9 +35,9 @@ class StVoteOutcomeTest(unittest.TestCase):
         in_df: pd.DataFrame = pd.DataFrame([
             {'yea': 0, 'nay': 1, 'supportRequiredPct': 15, 'minAcceptQuorum': 100000000000000000, 'votingPower': 5,'startDate': bl.unix()},#today_year-(today_month-3)-16T00:00:00+00:00
             {'yea': 6, 'nay': 2, 'supportRequiredPct': 50, 'minAcceptQuorum': 50, 'votingPower': 9,'startDate': bl.add(month=1).change(day=5).unix()},#today_year-(today_month-2)-05T00:00:00+00:00
-            {'yea': 1, 'nay': 0, 'supportRequiredPct': 100000000000000000, 'minAcceptQuorum': 33, 'votingPower': 3,'startDate': bl.unix()},#today_year-(today_month-2)-05T00:00:00+00:00
-            {'yea': 5, 'nay': 4, 'supportRequiredPct': 33, 'minAcceptQuorum': 15, 'votingPower': 9,'startDate': bl.add(month=2).change(day=1, hour=23, minute=59, second=59).unix()},#today_year-today_month-01T23:59:59+00:00
-            {'yea': 0, 'nay': 0, 'supportRequiredPct': 0, 'minAcceptQuorum': 0, 'votingPower': 00000000000000000,'startDate': bl.unix()},#today_year-today_month-01T23:59:59+00:00
+            {'yea': 1, 'nay': 0, 'supportRequiredPct': 100000000000000000, 'minAcceptQuorum': 330000000000000000, 'votingPower': 3,'startDate': bl.unix()},#today_year-(today_month-2)-05T00:00:00+00:00
+            {'yea': 5, 'nay': 4, 'supportRequiredPct': 330000000000000000, 'minAcceptQuorum': 150000000000000000, 'votingPower': 9,'startDate': bl.add(month=2).change(day=1, hour=23, minute=59, second=59).unix()},#today_year-today_month-01T23:59:59+00:00
+            {'yea': 0, 'nay': 0, 'supportRequiredPct': 0, 'minAcceptQuorum': 1, 'votingPower': 00000000000000000,'startDate': bl.unix()},#today_year-today_month-01T23:59:59+00:00
             {'yea': 4, 'nay': 0, 'supportRequiredPct': 1000000000000000000, 'minAcceptQuorum': 500000000000000000, 'votingPower': 10,'startDate': bl.add(second=1).change(day=27).unix()},#today_year-today_month-27T00:00:00+00:00
         ])
         out: List[List[int]] = [
