bbcf99a949741820802a75c4f38d2f99abcf08ad
David Davó <david@ddavo.me>
2021-10-15 18:02:04 +0200

    Fixed #30

--- cache_scripts/daohaus/collectors/moloch_collector.py
+++ cache_scripts/daohaus/collectors/moloch_collector.py
@@ -9,25 +9,55 @@
 
 import os
 import pandas as pd
+from tqdm import tqdm
 from typing import Dict, List
 from datetime import datetime, date
+from time import sleep
+import requests
+import logging
 
 from api_requester import ApiRequester
+import config
 
 
 MOLOCH_QUERY: str = '{{moloches(where: {{deleted: false}}, first: {0}, skip: {1}\
 ){{id title version summoner summoningTime timestamp proposalCount memberCount voteCount rageQuitCount totalGas}}}}'
 
 META_KEY: str = 'moloches'
+DATA_ENDPOINT: str = "https://data.daohaus.club/dao/{id}"
 
+def _request_moloch_name(moloch_id: str, retries=3) -> str:
+    for _ in range(retries):
+        response = requests.get(DATA_ENDPOINT.format(id=moloch_id))
+
+        if response:
+            o = response.json()
+            if isinstance(o, list) and o and "name" in o[0]:
+                return o[0]["name"]
+            else:
+                return None
+        else:
+            logging.warn(f"Couldn't get request, response {response}")
+            sleep(1)
 
 def _request_moloches(current_row: int, endpoint: str) -> List[Dict]:
     requester: ApiRequester = ApiRequester(endpoint=endpoint)
     print("Requesting MOLOCH\'s data ...")
     start: datetime = datetime.now()
 
-    moloches: List[Dict] = requester.n_requests(query=MOLOCH_QUERY, skip_n=current_row, 
+    moloches: List[Dict] = requester.n_requests(query=MOLOCH_QUERY, skip_n=current_row,
         result_key=META_KEY)
+
+    remaining_moloches = [m for m in moloches if not m["title"]]
+    if not config.skip_daohaus_names and remaining_moloches:
+        logging.info("Requesting remaining names from DAOHaus, this could take a while...")
+        failed = 0
+        for m in tqdm(remaining_moloches, unit="req"):
+            m["title"] = _request_moloch_name(m["id"])
+            failed += 1 if not m["title"] else 0
+
+        if failed > 0:
+            logging.warning(f"Failed to retrieve {failed} DAOs names")
 
     print(f'MOLOCH\'s data requested in {round((datetime.now() - start).total_seconds(), 2)}s')
     return moloches
--- cache_scripts/main.py
+++ cache_scripts/main.py
@@ -36,8 +36,28 @@ parser.add_argument(
     action='store_true', default=False,
     help="Removes the cache before updating"
 )
+parser.add_argument(
+    "--skip-daohaus-names",
+    action="store_true", default=False,
+    help="Skips the step of getting Daohaus Moloch's names, which takes some time"
+)
 
 logging.basicConfig(format=LOGGING_STR_FORMAT, level=logging.INFO)
+
+AVAILABLE_PLATFORMS = {
+    "aragon": aragon.run,
+    "daostack": daostack.run,
+    "daohaus": daohaus.run
+}
+
+def _call_platform(platform: str, force: bool=False):
+    platform_dir = os.path.join("datawarehouse", platform)
+    print("Platform_dir", platform_dir)
+    if force and os.path.isdir(platform_dir):
+        logging.warning(f"Removing path {platform_dir}")
+        shutil.rmtree(platform_dir)
+
+    AVAILABLE_PLATFORMS[platform]()
 
 if __name__ == '__main__':
     config.populate_args(parser.parse_args())
@@ -45,26 +65,20 @@ if __name__ == '__main__':
     if config.debug:
         logging.getLogger().setLevel(level=logging.DEBUG)
 
-    if config.force and os.path.isdir("datawarehouse"):
-        shutil.rmtree("datawarehouse")
-
-    available_platforms = {
-        "aragon": aragon.run,
-        "daostack": daostack.run,
-        "daohaus": daohaus.run
-    }
-
-    if not config.platforms:
-        for f in available_platforms.values():
-            f()
-
+    # Checking whether there is an unknown platform
     for p in config.platforms:
-        if p not in available_platforms.keys():
+        if p not in AVAILABLE_PLATFORMS.keys():
             print("Platform", p, "not found, available platforms are:")
-            print("  ", ", ".join(available_platforms.keys()))
+            print("  ", ", ".join(AVAILABLE_PLATFORMS.keys()))
             exit(1)
 
-        available_platforms[p]()
+    # The default config is every platform
+    if not config.platforms:
+        config.platforms = AVAILABLE_PLATFORMS.keys()
+
+    # Now calling the platform and deleting if needed
+    for p in config.platforms:
+        _call_platform(p, config.force)
 
     # write date
     data_date: str = str(date.today())
