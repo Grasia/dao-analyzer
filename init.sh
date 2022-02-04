173c5bbede8bc57c5f14c1cbe533d06ecfa0cdf2
David Davó <david@ddavo.me>
2022-02-04 10:07:21 +0100

    Fixed init.sh

--- init.sh
+++ init.sh
@@ -4,9 +4,8 @@ cache_scripts () {
     python cache_scripts/main.py --ignore-errors $DAOA_CACHE_ARGUMENTS
 }
 
-if [ -d "datawarehouse" ]; then
+if [ ! -d "datawarehouse" ]; then
     cache_scripts
-else
-    { sleep 60; cache_scripts; } &
 fi
+
 gunicorn index:server -c gunicorn_config.py
