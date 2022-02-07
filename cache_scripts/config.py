d94be168a222c068580b6df06b6e05e3d6c3e7ef
David Davó <david@ddavo.me>
2022-02-07 10:14:40 +0100

    Fixed SKIP_INVALID_BLOCKS

--- cache_scripts/config.py
+++ cache_scripts/config.py
@@ -4,7 +4,7 @@ CACHE_SCRIPTS_VERSION = "1.0.2"
 
 # https://letsexchange.io/blog/what-is-block-confirmation-on-ethereum-and-how-many-confirmations-are-required/
 # Number of blocks to skip to only consult confirmed blocks
-SKIP_INVALID_BLOCKS = 0
+SKIP_INVALID_BLOCKS = 250
 DEFAULT_DATAWAREHOUSE = Path('datawarehouse')
 
 __args = None
