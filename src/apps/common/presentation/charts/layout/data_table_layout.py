066f3e6cf96a40bc4e38cf0e80c8a828529b421f
David Davó <david@ddavo.me>
2022-03-17 12:08:24 +0100

    Fixed spinner color when loading table

--- src/apps/common/presentation/charts/layout/data_table_layout.py
+++ src/apps/common/presentation/charts/layout/data_table_layout.py
@@ -10,6 +10,7 @@ from dash import html, dcc
 from dash.dash_table import DataTable
 
 from . import ILayout
+from .data_table_configuration import DataTableConfiguration
 
 class DataTableLayout(ILayout):
     SUFFIX_ID_TABLE: str = '-dtable'
@@ -17,10 +18,15 @@ class DataTableLayout(ILayout):
     def __init__(self, title: str, css_id: str) -> None:
         self.__title: str = title
         self.__css_id: str = css_id
+        self.__configuration: DataTableConfiguration = DataTableConfiguration()
     
     @property
     def table_id(self):
         return f'{self.__css_id}{self.SUFFIX_ID_TABLE}'
+
+    @property
+    def configuration(self) -> DataTableConfiguration:
+        return self.__configuration
 
     def get_layout(self) -> html.Div:
         children = [
@@ -44,8 +50,7 @@ class DataTableLayout(ILayout):
         return html.Div(
             children=dcc.Loading(
                 type="circle",
-                # TODO: Use configuration color!!
-                # color=self.configuration.color,
+                color=self.configuration.color,
                 children=html.Div(
                     children=children,
                     id=self.__css_id,
@@ -55,7 +60,6 @@ class DataTableLayout(ILayout):
                 parent_style={'flex': '1 1 auto'},
                 style={'flex': '1 1 auto'}
             ),
-            # className=f'pane {self.configuration.css_border} two-column'
-            className=f'pane two-column',
+            className=f'pane {self.configuration.css_border} two-column',
             style={'align-self': 'stretch', 'display': 'flex'}
         )
