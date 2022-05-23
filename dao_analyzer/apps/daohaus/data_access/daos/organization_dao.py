167f573e17cc844fc7a5836ea639ef9d39a5f8c1
David Davó <david@ddavo.me>
2022-05-23 16:16:26 +0200

    Fixed memory bug

--- dao_analyzer/apps/daohaus/data_access/daos/organization_dao.py
+++ dao_analyzer/apps/daohaus/data_access/daos/organization_dao.py
@@ -11,44 +11,50 @@ import pandas as pd
 
 from dao_analyzer.apps.common.business.transfers import Organization, OrganizationList
 from dao_analyzer.apps.common.data_access.daos.organization_dao import OrganizationListDao
-from dao_analyzer.apps.common.data_access.requesters import JoinCacheRequester
+from dao_analyzer.apps.common.data_access.requesters import CacheRequester
 
 from .metric import srcs
 
 class DaohausDao(OrganizationListDao):
     __DF_IDX = ['network', 'molochAddress']
-    __DF_GB_COLS = __DF_IDX + ['name', 'summoningTime']
-    __DF_USED_COLS = __DF_GB_COLS + ['createdAt']
+    __DF_DATE = 'createdAt'
+    __DF_CLEAN_COLS = __DF_IDX + [__DF_DATE]
 
     def __init__(self):
-        super().__init__(JoinCacheRequester(srcs=[
+        super().__init__(CacheRequester(srcs=[
+            srcs.MOLOCHES,
+        ]))
+
+        self._activityRequester = CacheRequester([
             srcs.MOLOCHES,
             srcs.PROPOSALS,
             srcs.RAGE_QUITS,
             srcs.VOTES,
-        ], how='left', on=self.__DF_IDX))
+        ])
     
     def get_organizations(self) -> OrganizationList:
-        df: pd.DataFrame = self._requester.request()
+        df: pd.DataFrame = self._requester.request().set_index(self.__DF_IDX, drop=False)
+        activity = self._activityRequester.request()
 
-        df['createdAt'] = pd.to_datetime(df['createdAt'], unit='s')
+        # Clean df
+        activity = activity[self.__DF_CLEAN_COLS]
+        activity[self.__DF_DATE] = pd.to_datetime(activity[self.__DF_DATE], unit='s')
 
-        # Cleaning the df
-        df = df[self.__DF_USED_COLS]
 
-        dfgb = df.groupby(self.__DF_GB_COLS)
+        dfgb = activity.groupby(self.__DF_IDX)[self.__DF_DATE]
+        df['first_activity'] = dfgb.min()
+        df['last_activity'] = dfgb.max()
 
         l: OrganizationList = OrganizationList()
 
-
-        for idx, g in dfgb:
+        for _, org in df.iterrows():
             l.append(Organization(
-                network = idx[0],
-                o_id = idx[1],
-                name = idx[2],
-                creation_date = pd.to_datetime(idx[3], unit='s'),
-                first_activity = self._NaTtoNone(g['createdAt'].min()),
-                last_activity = self._NaTtoNone(g['createdAt'].max()),
+                network = org['network'],
+                o_id = org['molochAddress'],
+                name = org['name'],
+                creation_date = pd.to_datetime(org['summoningTime'], unit='s'),
+                first_activity = self._NaTtoNone(org['first_activity']),
+                last_activity = self._NaTtoNone(org['last_activity']),
             ))
 
         return l
