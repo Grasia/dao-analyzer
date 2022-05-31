67521c6a032fca4b225da99aeb8180f03fc03fe3
David Davó <david@ddavo.me>
2022-05-31 15:32:49 +0200

    Added DAOstack participation stats

--- dao_analyzer/apps/daostack/data_access/daos/organization_dao.py
+++ dao_analyzer/apps/daostack/data_access/daos/organization_dao.py
@@ -8,6 +8,7 @@
 import pandas as pd
 
 from dao_analyzer.apps.common.business.transfers import Organization, OrganizationList
+from dao_analyzer.apps.common.business.transfers.organization.participation_stats import MembersCreatedProposalsStat, MembersEverVotedStat
 from dao_analyzer.apps.common.data_access.daos.organization_dao import OrganizationListDao
 from dao_analyzer.apps.common.data_access.requesters import CacheRequester
 
@@ -20,6 +21,13 @@ class DaostackDAO(OrganizationListDao):
 
     __DF_CLEAN_COLS = __DF_IDX + [__DF_DATE]
 
+    __DF_PROPOSER = 'proposer'
+    __DF_PROP_COLS = __DF_IDX + [__DF_PROPOSER]
+    __DF_MEMBER = 'address'
+    __DF_MEMBER_COLS = __DF_IDX + [__DF_MEMBER]
+    __DF_VOTER = 'voter'
+    __DF_VOTES_COLS = __DF_IDX + [__DF_VOTER]
+
     def __init__(self):
         super().__init__(CacheRequester(srcs=[
             srcs.DAOS,
@@ -31,17 +39,62 @@ class DaostackDAO(OrganizationListDao):
             srcs.STAKES,
         ])
 
+        self._membersRequester = CacheRequester(srcs=[
+            srcs.REP_HOLDERS,
+        ])
+
+        self._votesRequester = CacheRequester(srcs=[
+            srcs.VOTES,
+        ])
+
+        self._propRequester = CacheRequester(srcs=[
+            srcs.PROPOSALS,
+        ])
+
     def get_organizations(self) -> OrganizationList:
         df: pd.DataFrame = self._requester.request().set_index(self.__DF_IDX, drop=False)
         activity: pd.DataFrame = self._activityRequester.request()
+        members = self._membersRequester.request()
+        prop = self._propRequester.request()
+        votes = self._votesRequester.request()
         
         # Clean df
         activity = activity[self.__DF_CLEAN_COLS]
         activity[self.__DF_DATE] = pd.to_datetime(activity[self.__DF_DATE], unit='s')
 
+        members = members[self.__DF_MEMBER_COLS]
+        prop = prop[self.__DF_PROP_COLS]
+        votes = votes[self.__DF_VOTES_COLS]
+
         dfgb = activity.groupby(self.__DF_IDX)[self.__DF_DATE]
         df['first_activity'] = dfgb.min()
         df['last_activity'] = dfgb.max()
+
+        # Only proposers that are members
+        prop = prop.merge(members,
+            left_on=self.__DF_IDX+[self.__DF_PROPOSER],
+            right_on=self.__DF_IDX+[self.__DF_MEMBER],
+        )[self.__DF_PROP_COLS]
+
+        # Only voters that are members
+        votes = votes.merge(members, 
+            left_on=self.__DF_IDX+[self.__DF_VOTER],
+            right_on=self.__DF_IDX+[self.__DF_MEMBER],
+        )[self.__DF_VOTES_COLS]
+
+        # Getting the participation
+        gbp = prop.groupby(self.__DF_IDX)
+        gbm = members.groupby(self.__DF_IDX)
+        gvt = votes.groupby(self.__DF_IDX)
+
+        # TODO: We should only consider proposers who are members
+        # -> We need to join them first, and select only proposer members?
+        # -> But how do we do that in a DAO by DAO basis without grouping?
+        df['mcp_pct'] = gbp[self.__DF_PROPOSER].nunique() / gbm[self.__DF_MEMBER].nunique()
+        # TODO: and voters who are members
+        df['mvt_pct'] = gvt[self.__DF_VOTER].nunique() / gbm[self.__DF_MEMBER].nunique()
+
+        df.reset_index(drop=True).to_feather('/tmp/aux30.arr')
 
         l: OrganizationList = OrganizationList()
 
@@ -53,6 +106,10 @@ class DaostackDAO(OrganizationListDao):
                 creation_date = None, # We don't know how to get it
                 first_activity = self._NaTtoNone(org['first_activity']),
                 last_activity = self._NaTtoNone(org['last_activity']),
+                participation_stats = [
+                    MembersCreatedProposalsStat(org['mcp_pct']),
+                    MembersEverVotedStat(org['mvt_pct']),
+                ],
             ))
 
         return l
